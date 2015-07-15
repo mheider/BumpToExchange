6daad2c4ffbdf31169f072c51bac6f556578f2bc
Markus H <hdrmrks254@googlemail.com>
2015-07-15 19:18:44 +0200

    Removed android. Added first example.

--- src/main/scala/de/hdrmrks/bmp/MainActivity.scala
+++ src/main/scala/de/hdrmrks/bmp/MainActivity.scala
@@ -1,34 +1,78 @@
 package de.hdrmrks.bmp;
 
 import internal.Compartment
-import android.app.Activity
-import android.os.Bundle
-import android.widget.FrameLayout
-import TypedResource._
-import android.util.Log
+import annotations.Role
 
-class MainActivity extends Activity with TypedFindView {
 
-  class BaseGreeter() {
-    def getGreeting(): String = "Hello World!"
+object BumpToExchangeExample extends App {
+
+  /** Simple class to hold our Resource, we want to share. */
+  case class Resource(data: Array[Byte], name: String)
+
+  class Device(name : String) {
+    override def toString(): String =  "Device: " + this.name
   }
 
-  class GreetingRole() {
-    def getGreeting(): String = "Hello World! SCROLL on Android Works!"
-  }
+  class Bump extends Compartment {
 
-  override def onCreate(b: Bundle) {
-    super.onCreate(b)
-    setContentView(R.layout.main)
-    val layout: FrameLayout = getLayoutInflater.inflate(TR.layout.main, null)
-
-    Log.e("MainActivity", "Init Compartment")
-    new Compartment {
-      Log.e("MainActivity", "Init play")
-      var greeter = new BaseGreeter();
-      Log.e("MainActivity", "Init greeting")
-      val greeting: String = greeter.getGreeting()
-      findView(TR.test_textview).setText(greeting)
+    class RessourceProvider {
+      def getResource() : Resource = {
+        val file = "/Users/markus/Desktop/hello.txt"
+        val data = scala.io.Source.fromFile(file).map(_.toByte).toArray
+        return new Resource(data, "hello.txt")
+      }
     }
+
+  }
+
+
+  class Exchange(resource: Resource) extends Compartment {
+
+    def execute() {
+      println("Executing exchange")
+      val sender = one[Sender]()
+      val receiver = one[Receiver]()
+      println("Sending from " + (+sender).name + " to " + (+receiver).name)
+      sender.send(receiver, resource)
+    }
+
+    @Role class Receiver() {
+      def receive(res : Resource): Unit = {
+        val dataStr = new String(res.data, "UTF-8")
+        println("FileName: " + res.name + " ################")
+        println("Content START #############################")
+        print(dataStr)
+        println("Content END ###############################")
+      }
+    }
+
+    @Role class Sender() {
+      def send(receiver: Receiver, res : Resource) = {
+        receiver.receive(res)
+      }
+    }
+
+  }
+
+  val iPhone = new Device("iPhone6")
+  val galS5  = new Device("Galaxy S5")
+
+  new Bump {
+
+    val resProv = new RessourceProvider()
+    val resource = resProv.getResource()
+
+    val exchange = new Exchange(resource)
+
+    val receiver = new exchange.Receiver()
+    val sender = new exchange.Sender()
+
+    (iPhone.play(receiver))
+    (galS5.play(sender))
+
+    exchange partOf(this)
+
+    exchange.execute()
   }
 }
+
