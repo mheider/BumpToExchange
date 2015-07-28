a3313c789e4746ddeedf409b4519753a0fe91126
Markus Heider <hdrmrks254@googlemail.com>
2015-07-28 10:20:56 +0200

    More Abstraction in Bump Compartment

---
+++ src/main/scala/de/hdrmrks/bmp/ResourceProvider.scala
@@ -0,0 +1,8 @@
+package de.hdrmrks.bmp
+
+import annotations.Role
+
+/**
+ * Created by Markus on 28.07.2015.
+ */
+
---
+++ src/main/scala/de/hdrmrks/bmp/example/PCBump.scala
@@ -0,0 +1,10 @@
+package de.hdrmrks.bmp.example
+
+import de.hdrmrks.bmp.Bump
+
+/**
+ * Created by Markus on 28.07.2015.
+ */
+class PCBump extends Bump{
+
+}
